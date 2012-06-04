a557032f343586ed15779d4548fb781abf413918
Mahipal Raythattha <mahipalr@gmail.com>
2012-06-04 15:45:13 -0700

    Initial commit. New project.

---
+++ MRStackExample/SEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SEAppDelegate.h
+//  MRStackExample
+//
+//  Created by Mahipal Raythattha on 6/4/12.
+//  Copyright (c) 2012 Mahipal Raythattha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MRStackExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MRStackExample
+//
+//  Created by Mahipal Raythattha on 6/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SEAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SEAppDelegate class]));
+    }
+}
